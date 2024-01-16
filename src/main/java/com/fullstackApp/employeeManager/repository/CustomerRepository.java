5acdfce4d348bf99e52da889448efbf74eedf371
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-01-16 09:57:26 +0300

    Added repositories for my classes

---
+++ src/main/java/com/fullstackApp/employeeManager/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.fullstackApp.employeeManager.repository;
+
+import com.fullstackApp.employeeManager.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
